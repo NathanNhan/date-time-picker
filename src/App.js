44a98bdd8963b0795b0d19f99deebb6e60f5cfc9
NathanNhan <trongnhan8150@gmail.com>
2022-01-21 16:22:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import DateFnsUtils from '@date-io/date-fns'; // choose your lib
+import {
+  DatePicker,
+  TimePicker,
+  DateTimePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
 
 function App() {
+  const [selectedDate, handleDateChange] = useState(localStorage.getItem("date") ? JSON.parse(localStorage.getItem("date")) : new Date());
+  useEffect(()=>{
+    localStorage.setItem("date", JSON.stringify(selectedDate));
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <DatePicker value={selectedDate} onChange={handleDateChange} />
+      <TimePicker value={selectedDate} onChange={handleDateChange} />
+      <DateTimePicker value={selectedDate} onChange={handleDateChange} />
+    </MuiPickersUtilsProvider>
   );
 }
 
